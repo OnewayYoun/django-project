84dcb0bc22b6957e95885a2dadf6d7d44cc27129
OnewayYoun <onewayy91@gmail.com>
2021-11-03 09:50:43 +0900

    post method

--- blog/weather/views.py
+++ blog/weather/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def weather(request):
-    return render(request, 'weather.html')
+    if request.method == 'POST':
+        city = request.POST['city']
+    else:
+        city = ''
+    return render(request, 'weather.html', dict(city=city))
