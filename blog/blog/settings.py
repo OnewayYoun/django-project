07f35840c283989e4bdfccda561808b82bfbb57f
OnewayYoun <onewayy91@gmail.com>
2021-11-04 10:44:16 +0900

    Implemented weather func with (Weather API - OpenWeatherMap)

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os, json
 from django.core.exceptions import ImproperlyConfigured
+from onewayy.hide import get_secrets
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,21 +22,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-secret_file = os.path.join(BASE_DIR, 'secrets.json')
+# secret_file = os.path.join(BASE_DIR, 'secrets.json')
+#
+# with open(secret_file) as f:
+#     secrets = json.loads(f.read())
+#
+#
+# def get_secret(setting, secrets=secrets):
+#     try:
+#         return secrets[setting]
+#     except KeyError:
+#         error_msg = "Set the {} environment variable".format(setting)
+#         raise ImproperlyConfigured(error_msg)
 
-with open(secret_file) as f:
-    secrets = json.loads(f.read())
 
-
-def get_secret(setting, secrets=secrets):
-    try:
-        return secrets[setting]
-    except KeyError:
-        error_msg = "Set the {} environment variable".format(setting)
-        raise ImproperlyConfigured(error_msg)
-
-
-SECRET_KEY = get_secret("SECRET_KEY")
+SECRET_KEY = get_secrets("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -53,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'posts',
     'weather',
+    'onewayy',
 ]
 
 MIDDLEWARE = [
--- blog/weather/views.py
+++ blog/weather/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render, HttpResponse
+from onewayy.hide import get_secrets
+import requests
 
 
 # Create your views here.
 def weather(request):
+    api_key = get_secrets('OpenWeather_API_KEY')
     if request.method == 'POST':
         city = request.POST['city']
+        base_url = 'https://api.openweathermap.org/data/2.5/weather'
+        r = requests.get(f'{base_url}', params=dict(q=city, appid=api_key, units='metric'))
+        if r.status_code == 200:
+            res = r.json()
+            data = dict(
+                temp=str(res['main']['temp']) + 'C',
+                pressure=str(res['main']['pressure']),
+                humidity=str(res['main']['humidity']),
+                coordinate=str(res['coord']['lon']) + ', ' + str(res['coord']['lat']),
+                country_code=str(res['sys']['country']),
+            )
+        else:
+            message = 'Invalid City Name. Please try again'
+            return render(request, 'weather.html', dict(message=message))
     else:
         city = ''
-    return render(request, 'weather.html', dict(city=city))
+        data = {}
+
+    return render(request, 'weather.html', dict(city=city, data=data))
