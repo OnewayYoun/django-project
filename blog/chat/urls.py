828c8f3373c921d980f91b29936f7a5dc1504f72
OnewayYoun <onewayy91@gmail.com>
2021-11-05 16:55:48 +0900

    update view(check if objects are in the model)

--- blog/chat/urls.py
+++ blog/chat/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.chat, name='chat')
+    path('', views.chat, name='chat'),
+    path('<str:room>/', views.room, name='room'),
+    path('checkview', views.checkview, name='checkview'),
 ]
