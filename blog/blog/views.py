fb46ea1fdb99af05d9fe8b58741b73966c4f88b4
OnewayYoun <onewayy91@gmail.com>
2021-11-16 00:50:42 +0900

    testing djangorestframework

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from chat.models import Room
 
 
 class TestView(APIView):
     def get(self, request, *args, **kwargs):
         sample_data = dict(username='sample', active_years_for=10)
-        return Response(sample_data)
+        room_name = Room.objects.all()
+        sample = room_name.values()
+        # return Response(sample)
+        return Response(dict(name_of_room=list(sample)))
+        # return Response(sample_data)
