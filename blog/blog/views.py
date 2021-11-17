e8ee01b844f24b975ed05d19b9f55a28804a9088
OnewayYoun <onewayy91@gmail.com>
2021-11-17 23:33:34 +0900

    testing serializer

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -7,18 +7,19 @@ from onewayydrf.models import Student
 
 
 class TestView(APIView):
-    # def get(self, request, *args, **kwargs):
-    #     sample_data = dict(username='sample', active_years_for=10)
-    #     room_name = Room.objects.all()
-    #     sample = room_name.values()
-    #     # return Response(sample)
-    #     return Response(dict(name_of_room=list(sample)))
-    #     # return Response(sample_data)
+    """
+    def get(self, request, *args, **kwargs):
+        sample_data = dict(username='sample', active_years_for=10)
+        room_name = Room.objects.all()
+        sample = room_name.values()
+        # return Response(sample)
+        return Response(dict(name_of_room=list(sample)))
+        # return Response(sample_data)
+    """
     def get(self, request, *args, **kwargs):
         qs = Student.objects.all()
         serializer = StudentSerializer(qs, many=True)
         return Response(serializer.data)
-
 
     def post(self, request, *args, **kwargs):
         serializer = StudentSerializer(data=request.data)
